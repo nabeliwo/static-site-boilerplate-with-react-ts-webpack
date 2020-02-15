5029b22a33349b8efe68b72bde127f0ca3fcea7b
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-02-15 17:58:00 +0900

    :zap: optimize production file

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -2,8 +2,8 @@
 
 const { resolve } = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const merge = require('webpack-merge')
 
 const base = require('./webpack.config')
@@ -12,7 +12,8 @@ const productionConfig = {
   mode: 'production',
   entry: resolve(__dirname, '../src/index.tsx'),
   output: {
-    filename: 'assets/js/bundle.js',
+    filename: 'assets/js/[name].[chunkhash].bundle.js',
+    chunkFilename: 'assets/js/[name].[chunkhash].[id].bundle.js',
     path: resolve(__dirname, '../public'),
   },
   plugins: [
@@ -24,6 +25,23 @@ const productionConfig = {
     ]),
     new HtmlWebpackPlugin({ template: resolve(__dirname, '../static/index.html') }),
   ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+      }),
+    ],
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+          enforce: true,
+        },
+      },
+    },
+  },
 }
 
 module.exports = merge(base, productionConfig)
